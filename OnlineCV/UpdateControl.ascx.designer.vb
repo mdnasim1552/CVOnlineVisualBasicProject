60d85fbd135a09738f8313f15650969d72ca21e7
Md. Nasim Uddin <md.nasim1552@gmail.com>
2024-07-07 23:57:51 +0600

    Ajax get type

--- OnlineCV/UpdateControl.ascx.designer.vb
+++ OnlineCV/UpdateControl.ascx.designer.vb
@@ -14,6 +14,15 @@ Option Explicit On
 Partial Public Class UpdateControl
 
     '''<summary>
+    '''Button1 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
     '''lnkbtnRefresh control.
     '''</summary>
     '''<remarks>
